3ca4d20f47ce2bf7765deca526e129fea97dc403
makrotheeast <makrotheeast@gmail.com>
2023-03-12 16:35:35 +0300

    Refactored two functions

--- task 5/print_even_values.py
+++ task 5/print_even_values.py
@@ -2,11 +2,9 @@ def print_even_values(collection: list):
     if len(collection) == 0:
         return
 
-    if len(collection) == 1:
-        if collection[0] % 2 == 0:
-            print(collection[0])
-        return
-
     if collection[0] % 2 == 0:
         print(collection[0])
+        if len(collection) == 1:
+            return
+
     print_even_values(collection[1:])
