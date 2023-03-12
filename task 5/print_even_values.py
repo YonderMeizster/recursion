06fbf705fb492a4e896bf090d688c7aa2798d69d
makrotheeast <makrotheeast@gmail.com>
2023-03-12 14:40:58 +0300

    Added checking about equality the length to 1

--- task 5/print_even_values.py
+++ task 5/print_even_values.py
@@ -2,6 +2,11 @@ def print_even_values(collection: list):
     if len(collection) == 0:
         return
 
+    if len(collection) == 1:
+        if collection[0] % 2 == 0:
+            print(collection[0])
+        return
+
     if collection[0] % 2 == 0:
         print(collection[0])
     print_even_values(collection[1:])
--- task 6/print_even_indexes.py
+++ task 6/print_even_indexes.py
@@ -2,5 +2,9 @@ def print_even_indexes(collection : list):
     if len(collection) == 0:
         return
 
+    if len(collection) == 1:
+        print(collection[0])
+        return
+
     print(collection[0])
     print_even_indexes(collection[2:])
