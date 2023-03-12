bebbef8ef270fc72c74893a1df940428955019a5
makrotheeast <makrotheeast@gmail.com>
2023-03-12 11:51:25 +0300

    Fixed little mistake

--- task 3/list_length.py
+++ task 3/list_length.py
@@ -1,6 +1,6 @@
 def estimate_length(collection : list):
     if len(collection) > 0:
         temp_copy = collection[:]
-        temp_copy.pop()
+        temp_copy.pop(0)
         return 1 + estimate_length(temp_copy)
     return 0
